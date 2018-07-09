5d5f16948fb47bb5c34cfa3b836d34409ff59c94
Vadym.Kozak <vadim.kozak.it@gmail.com>
2018-07-09 12:41:19 +0300

    COUPONF-1: code clean up

--- src/modules/coupons/components/coupon-grid/coupon-grid.component.ts
+++ src/modules/coupons/components/coupon-grid/coupon-grid.component.ts
@@ -16,16 +16,16 @@ export class CouponGridComponent extends DataGridAbstractComponent<any> {
   viewMode: DataViewModeType = 'list';
   pageNumber: number = 0;
   selectedItems: any[] = [];
+  searchValue: string;
   chips: DataGridFilterInterface[] = [];
   columns: DataGridTableColumnInterface[] = [
-    { name: 'title', title: 'Name', isActive: true, isToggleable: true },
-    { name: 'title1', title: 'Code', isActive: true, isToggleable: true },
-    { name: 'title2', title: 'Expiry Date', isActive: true, isToggleable: true },
-    { name: 'title3', title: 'No. Used', isActive: true, isToggleable: true },
+    { name: 'name', title: 'Name', isActive: true, isToggleable: true },
+    { name: 'code', title: 'Code', isActive: true, isToggleable: true },
+    { name: 'expiry_date', title: 'Expiry Date', isActive: true, isToggleable: true },
+    { name: 'no_used', title: 'No. Used', isActive: true, isToggleable: true },
     { name: 'selected', title: 'Enabled', isActive: true, isToggleable: true },
     { name: 'menu', title: '', isActive: true, isToggleable: true }
   ];
-  searchValue: string;
 
   get activeColumns(): string[] {
     return this.columns
@@ -43,6 +43,27 @@ export class CouponGridComponent extends DataGridAbstractComponent<any> {
   ngOnInit(): void {
     super.ngOnInit();
     this.fetchProducts(0);
+  }
+
+  fetchProducts(page: number, search?: string): void {
+    this.selectedItems = [];
+    let url: string = `https://stage.payever.de/products/api/v1/products?f%5Bbusiness%5D%5Bv%5D%5B%5D=b197bf22-6309-11e7-a2a8-5254008319f0&limit=${this.pageSize}&page=${page || this.pageNumber}`;
+
+    this.pageNumber = page || this.pageNumber;
+    if (Boolean(search) || search === '') {
+      this.searchValue = search;
+      url += `&f[search]=${search}`;
+    }
+
+    this.httpClient.get(url)
+      .subscribe((products: any) => {
+        this.items = products.collection;
+        this.allItemsCount = products.pagination.item_count;
+      });
+  }
+
+  sortData(event: any): void {
+    console.log(event);
   }
 
   onRowSelected(item: any): void {
@@ -64,28 +85,7 @@ export class CouponGridComponent extends DataGridAbstractComponent<any> {
     }
   }
 
-  toggleClick(event: any) {
+  onToggleClick(event: any) {
     event.stopPropagation();
-  }
-
-  sortData(event: any): void {
-    console.log(event);
-  }
-
-  fetchProducts(page: number, search?: string): void {
-    this.selectedItems = [];
-    let url: string = `https://stage.payever.de/products/api/v1/products?f%5Bbusiness%5D%5Bv%5D%5B%5D=b197bf22-6309-11e7-a2a8-5254008319f0&limit=${this.pageSize}&page=${page || this.pageNumber}`;
-
-    this.pageNumber = page || this.pageNumber;
-    if (Boolean(search) || search === '') {
-      this.searchValue = search;
-      url += `&f[search]=${search}`;
-    }
-
-    this.httpClient.get(url)
-      .subscribe((products: any) => {
-        this.items = products.collection;
-        this.allItemsCount = products.pagination.item_count;
-      });
   }
 }
